d6d69f838889360d35c42ae572fd1c82069ec6e6
Daira Hopwood <daira@jacaranda.org>
2022-02-08 14:56:48 +0000

    Merge files that were only separate in order to provide multiple entry points.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- zcash_test_vectors/orchard/group_hash.py
+++ zcash_test_vectors/orchard/group_hash.py
@@ -7,10 +7,12 @@ import math
 from . import iso_pallas
 
 from .pallas import Fp, p, q, PALLAS_B, Point
-from .iso_pallas import PALLAS_ISO_B, PALLAS_ISO_A
-from ..utils import i2beosp, cldiv, beos2ip, i2leosp, lebs2ip
+from .iso_pallas import PALLAS_ISO_B, PALLAS_ISO_A, Point as IsoPoint
+
+from ..utils import i2beosp, cldiv, beos2ip, i2leosp, lebs2ip, leos2ip
 from ..output import render_args, render_tv
 from ..rand import Rand
+
 
 # https://stackoverflow.com/questions/2612720/how-to-do-bitwise-exclusive-or-of-two-strings-in-python
 def sxor(s1,s2):
@@ -180,6 +182,49 @@ def main():
             'msg': msg,
             'point': bytes(group_hash(domain, msg)),
         } for (domain, msg) in test_vectors],
+    )
+
+def map_to_curve_test_vectors():
+    fixed_test_vectors = [
+        (Fp(0), IsoPoint(Fp(19938918781445865934736160264407396416050199005817793816893455093350997047296),
+                         Fp(1448774895934493446148762800986014913165975534940595774801697325542407056356))),
+        (Fp(1), IsoPoint(Fp(5290181550357368025040301950220623271393946308300025648720253222947454165280),
+                         Fp(24520995241805476578231005891941079870703368870355132644748659103632565232759))),
+        (Fp(0x123456789abcdef123456789abcdef123456789abcdef123456789abcdef0123),
+                IsoPoint(Fp(16711718778908753690082328243251803703269853000652055785581237369882690082595),
+                         Fp(1764705856161931038824461929646873031992914829456409784642560948827969833589))),
+    ]
+
+    for (u, point) in fixed_test_vectors:
+        P = map_to_curve_simple_swu(u)
+        assert P == point
+
+    test_vectors = [u for (u, _) in fixed_test_vectors]
+
+    from random import Random
+    rng = Random(0xabad533d)
+    def randbytes(l):
+        ret = []
+        while len(ret) < l:
+            ret.append(rng.randrange(0, 256))
+        return bytes(ret)
+    rand = Rand(randbytes)
+
+    # Generate random test vectors
+    for _ in range(10):
+        test_vectors.append(Fp(leos2ip(rand.b(32))))
+
+    render_tv(
+        render_args(),
+        'orchard_map_to_curve',
+        (
+            ('u', '[u8; 32]'),
+            ('point', '[u8; 32]'),
+        ),
+        [{
+            'u': bytes(u),
+            'point': bytes(map_to_curve_simple_swu(u)),
+        } for u in test_vectors],
     )
 
 
