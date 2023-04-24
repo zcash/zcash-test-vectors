ec060090ae0aeaf3280dd26e459d82f8efbe6797
Constance Beguier <constance@qed-it.com>
2023-04-24 12:20:05 +0200

    Update R constant for ZSA note commitment (#8)
    We would like to have the same R constant for ZEC and ZSA note commitments.

--- zcash_test_vectors/orchard/commitments.py
+++ zcash_test_vectors/orchard/commitments.py
@@ -31,6 +31,13 @@ def sinsemilla_commit(r: Scalar, D, M):
         group_hash(D + b"-r", b"") * r
     )
 
+# https://zips.z.cash/protocol/nu5.pdf#concretesinsemillacommit
+def sinsemilla_commit_with_blind_personalization(r: Scalar, D_hash, D_blind, M):
+    assert isinstance(r, Scalar)
+    return sinsemilla_hash_to_point(D_hash + b"-M", M) + (
+        group_hash(D_blind + b"-r", b"") * r
+    )
+
 def sinsemilla_short_commit(r: Scalar, D, M):
     return sinsemilla_commit(r, D, M).extract()
 
@@ -50,9 +57,10 @@ def note_commit_orchard(rcm, g_d, pk_d, v, rho, psi):
     )
 
 def note_commit_zsa(rcm, g_d, pk_d, v, asset, rho, psi):
-    return sinsemilla_commit(
+    return sinsemilla_commit_with_blind_personalization(
         rcm,
         b"z.cash:ZSA-NoteCommit",
+        b"z.cash:Orchard-NoteCommit",
         g_d + pk_d + i2lebsp(64, v) + i2lebsp(L_ORCHARD_BASE, rho.s) + i2lebsp(L_ORCHARD_BASE, psi.s) + asset
     )
 
