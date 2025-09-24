dcee947e0e65805afa38b8dc9b5b5bc404335560
Jack Grigg <jack@electriccoin.co>
2025-09-24 14:59:39 +0000

    Move address constants out of ZIP 320 test generator

--- zcash_test_vectors/hd_common.py
+++ zcash_test_vectors/hd_common.py
@@ -3,6 +3,24 @@
 ZCASH_MAIN_COINTYPE = 133
 ZCASH_TEST_COINTYPE = 1
 
+ADDRESS_CONSTANTS = {
+    "mainnet": {
+        "coin_type": ZCASH_MAIN_COINTYPE,
+        "p2pkh_lead": [0x1c, 0xb8],
+        "tex_hrp": "tex",
+    },
+    "testnet": {
+        "coin_type": ZCASH_TEST_COINTYPE,
+        "p2pkh_lead": [0x1d, 0x25],
+        "tex_hrp": "textest",
+    },
+    "regtest": {
+        "coin_type": ZCASH_TEST_COINTYPE,
+        "p2pkh_lead": [0x1d, 0x25],
+        "tex_hrp": "texregtest",
+    },
+}
+
 def hardened(i):
     assert 0 <= i and i < (1<<31)
     return i + (1<<31)
