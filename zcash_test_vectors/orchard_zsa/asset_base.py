c461293331cb631c18e6ab7d6d302344d47ec0d5
Vivek Arte <46618816+vivek-arte@users.noreply.github.com>
2026-02-02 19:42:10 +0530

    Expand the sample set of Unicode code points (#47)
    This expands the sampled code points for the get_random_unicode_bytes function to include all allowed Unicode code points.
    
    This changes the random values of the description of the Assets, and therefore changes the orchard_zsa_digests.rs and orchard_zsa_asset_base.rs files, which would need to be copied into the librustzcash and orchard repositories respectively.

--- zcash_test_vectors/orchard_zsa/asset_base.py
+++ zcash_test_vectors/orchard_zsa/asset_base.py
@@ -47,21 +47,12 @@ def get_random_unicode_bytes(length, rand):
 
     random.seed(rand.u8())
 
-    # TODO: Update this to include code point ranges to be sampled
+    # We include ALL valid Unicode code points for sampling.
     include_ranges = [
-        ( 0x0021, 0x0021 ),
-        ( 0x0023, 0x0026 ),
-        ( 0x0028, 0x007E ),
-        ( 0x00A1, 0x00AC ),
-        ( 0x00AE, 0x00FF ),
-        ( 0x0100, 0x017F ),
-        ( 0x0180, 0x024F ),
-        ( 0x2C60, 0x2C7F ),
-        ( 0x16A0, 0x16F0 ),
-        ( 0x0370, 0x0377 ),
-        ( 0x037A, 0x037E ),
-        ( 0x0384, 0x038A ),
-        ( 0x038C, 0x038C ),
+        (0x0000, 0xD7FF),
+        (0xE000, 0xFDCF),
+        (0xFDF0, 0xFFFD),
+        (0x10000, 0x10FFFD),
     ]
 
     alphabet = [
